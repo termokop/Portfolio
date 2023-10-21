a5d35d36e2e121b9f8859232b4d2450debe84607
termokop <termokop564@gmail.com>
2023-10-21 12:01:19 -0600

    add links component

---
+++ src/components/vLinks.vue
@@ -0,0 +1,78 @@
+<script setup>
+
+// import { ref } from 'vue';
+import IconSoloLearn from './icons/IconSoloLearn.vue'
+import IconCodeWars from './icons/IconCodeWars.vue'
+import IconGitHub from './icons/IconGitHub.vue'
+import IconFacebook from './icons/IconFacebook.vue'
+import IconTelegram from './icons/IconTelegram.vue'
+import IconLinkedIn from './icons/IconLinkedIn.vue'
+import IconGMail from './icons/IconGMail.vue'
+
+</script>
+
+<template>
+
+    <div class="links">
+        <h3>You can see my code here:</h3>
+        <ul>
+            <li>
+                <a href="https://www.sololearn.com/profile/27120555" target="_blank"><IconSoloLearn></IconSoloLearn> <br> SoloLearn</a>    
+            </li>    
+            <li>
+                <a href="https://www.codewars.com/users/termokop" target="_blank"><IconCodeWars></IconCodeWars> <br> CodeWars</a>    
+            </li>
+            <li>
+                <a href="https://github.com/termokop" target="_blank"><IconGitHub></IconGitHub> <br> GitHub</a>    
+            </li>
+        </ul>
+
+        <h3>You can contact me here:</h3>
+        <ul class="contacts">
+            <li>
+                <a class="green" href="https://www.facebook.com/termokop" target="_blank"><IconFacebook></IconFacebook></a>    
+            </li>    
+            <li>
+                <a class="green" href="https://t.me/termokop" target="_blank"><IconTelegram></IconTelegram></a>    
+            </li>  
+            <li>
+                <a class="green" href="https://www.linkedin.com/in/yurii-hruba-085b7a22b/" target="_blank"><IconLinkedIn></IconLinkedIn></a>    
+            </li> 
+            <li>
+                <a class="green" href="mailto:termokop564@gmail.com" target="_blank"><IconGMail></IconGMail></a>    
+            </li> 
+        </ul>
+    </div>
+
+</template>
+
+<style scoped>
+
+.links {
+    width: 30%;
+    margin: auto;
+}
+
+ul {
+    padding: 0;
+}
+
+li{
+    display: inline;
+}
+a {
+    outline: none;
+    text-decoration: none;
+    display: inline-block;
+    margin-left: 1%;
+    text-align: center;
+    padding: 5px;
+    box-sizing: border-box;
+    width: 31%;
+}
+
+.contacts li a {
+    width: 24%;
+}
+
+</style>
